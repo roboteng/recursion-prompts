f947d98a3ba678df6439d8bab77588e30b1ad24e
Trevor Settles <roboteng@gmail.com>
2021-07-12 10:48:42 -0600

    implemented palindrome for single words

--- src/recursion.js
+++ src/recursion.js
@@ -116,7 +116,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
-};
+  if (string.length === 1 || string.length === 0 ) {
+    return true;
+  }
+  if (string[0] === string[string.length-1]) {
+    return palindrome(string.substring(1, length-1));
+  }
+  return false;
+}
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
