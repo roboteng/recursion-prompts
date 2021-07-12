9b2358d4d256cfa8a64e3b2fc81478fc332f280a
Trevor Settles <roboteng@gmail.com>
2021-07-12 10:58:06 -0600

    implemented mulitply

--- src/recursion.js
+++ src/recursion.js
@@ -150,6 +150,16 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (x === 0 || y === 0) {
+    return 0;
+  }
+  if (x < 0) {
+    return -multiply(-x, y);
+  }
+  if (y < 0) {
+    return -multiply(x, -y);
+  }
+  return y + multiply(x - 1, y);
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
