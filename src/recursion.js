6abc7f877b4721d2bad3aac12b01f1d8eca2393d
Trevor Settles <roboteng@gmail.com>
2021-07-12 10:11:02 -0600

    imolemented arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -28,6 +28,15 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (!Array.isArray(array)) {
+    return array;
+  }
+  var sum = 0;
+  array.forEach( function (element) {
+      sum += arraySum(element);
+    }
+  );
+  return sum
 };
 
 // 4. Check if a number is even.
