224b12d9607f1834695f4f14521861c87ce5bf0d
Trevor Settles <roboteng@gmail.com>
2021-07-12 10:38:32 -0600

    implemented reverseString

--- src/recursion.js
+++ src/recursion.js
@@ -108,6 +108,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return string;
+  }
+  return reverse(string.substring(1)) + string[0];
 };
 
 // 10. Write a function that determines if a string is a palindrome.
