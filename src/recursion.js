95165085fc71014c4ca5f9738b773086ad96a687
Trevor Settles <roboteng@gmail.com>
2021-07-12 13:35:13 -0600

    implemented CompareStrings

--- src/recursion.js
+++ src/recursion.js
@@ -203,6 +203,10 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+  return (str1[0] === str2[0]) && compareStr(str1.substring(1), str2.substring(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
